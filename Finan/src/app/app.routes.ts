a03cd3020dcd5a72021ad89e8071d7cc2b2992c3
Marcelle Nascimento <marcellenascim@gmail.com>
2026-05-25 16:57:31 -0300

    pronto

--- Finan/src/app/app.routes.ts
+++ Finan/src/app/app.routes.ts
@@ -2,25 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'cadastro',
-    loadComponent: () => import('./cadastro/cadastro.page').then(m => m.CadastroPage)
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./login/login.page').then(m => m.LoginPage)
-  },
-  {
-    path: 'casa',
-    loadComponent: () => import('./casa/casa.page').then(m => m.CasaPage)
-  },
-  {
     path: '',
-    redirectTo: 'cadastro', // Define que ao abrir o app, vai para o cadastro
-    pathMatch: 'full'
+    redirectTo: 'home', // Alterado de 'inicio' para 'home'
+    pathMatch: 'full',
   },
   {
     path: 'home',
-    redirectTo: 'casa', // Redireciona caso algum código antigo ainda tente ir para /home
-    pathMatch: 'full'
-  }
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'cadastro',
+    loadComponent: () => import('./cadastro/cadastro.page').then( m => m.CadastroPage)
+  },
+  {
+    path: 'casa',
+    loadComponent: () => import('./casa/casa.page').then( m => m.CasaPage)
+  },
 ];
--- Finan/src/environments/environment.ts
+++ Finan/src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false, // <--- A VÍRGULA QUE ESTAVA FALTANDO AQUI
   firebaseConfig: {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    projectId: "SEU_PROJETO",
-    storageBucket: "SEU_PROJETO.appspot.com",
-    messagingSenderId: "ID",
-    appId: "ID_DO_APP"
+    apiKey: "AIzaSyDG4xIKzcpxa1YwXjgUje2Wus-fVyOqaeU",
+  authDomain: "finan-97083.firebaseapp.com",
+  projectId: "finan-97083",
+  storageBucket: "finan-97083.firebasestorage.app",
+  messagingSenderId: "793013836052",
+  appId: "1:793013836052:web:c8ef8c900ae5d404c04a01"
+
   }
 };
