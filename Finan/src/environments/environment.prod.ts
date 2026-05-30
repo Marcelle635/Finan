3a262ad431bb3d654a8116663570c255b0919d63
Marcelle Nascimento <marcellenascim@gmail.com>
2026-05-30 19:51:23 -0300

    pronto

--- Finan/src/environments/environment.prod.ts
+++ Finan/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false, // <--- A VÍRGULA QUE ESTAVA FALTANDO AQUI
+  firebaseConfig: {
+    apiKey: "AIzaSyDG4xIKzcpxa1YwXjgUje2Wus-fVyOqaeU",
+  authDomain: "finan-97083.firebaseapp.com",
+  projectId: "finan-97083",
+  storageBucket: "finan-97083.firebasestorage.app",
+  messagingSenderId: "793013836052",
+  appId: "1:793013836052:web:c8ef8c900ae5d404c04a01"
+
+  }
 };
